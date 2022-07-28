6b6da2856dcba640e03591bfbcbb85c4a804b97a
Kevin Kendle <1066413+KKendle@users.noreply.github.com>
2022-07-28 16:09:11 -0500

    randomize enemy attack target

--- js/enemy.js
+++ js/enemy.js
@@ -4,6 +4,22 @@ class Enemy extends Character {
     }
 
     takeTurn() {
-        this.attack({self: this, target: charactersInBattle[2]});
+        /* find player and player allies */
+        const potentialTargets = [];
+        const player = document.querySelector('.player');
+        const playerAllies = document.querySelectorAll('.ally');
+
+        /* add player and allies to array of potential targets to attack */
+        potentialTargets.push(player);
+        playerAllies.forEach(playerAlly => {
+            potentialTargets.push(playerAlly);
+        })
+
+        // choose a random target to attack
+        const attackTargetPosition = Math.floor(Math.random() * potentialTargets.length);
+        const attackTarget = targetCharacter({target: potentialTargets[attackTargetPosition]});
+
+        // attack
+        this.attack({self: this, target: charactersInBattle[attackTarget]});
     }
 }

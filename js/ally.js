18f81672fa8ba1122153cc0130cefc2e1c385c8d
Kevin Kendle <1066413+KKendle@users.noreply.github.com>
2022-07-28 16:15:17 -0500

    add allies attacking random enemies

--- js/ally.js
+++ js/ally.js
@@ -5,6 +5,20 @@ class Ally extends Character {
     }
 
     takeTurn() {
-        this.attack({self: this, target: charactersInBattle[1]});
+        /* find enemy allies */
+        const potentialTargets = [];
+        const enemyAllies = document.querySelectorAll('.enemy');
+
+        /* add enemy allies to array of potential targets to attack */
+        enemyAllies.forEach(enemyAlly => {
+            potentialTargets.push(enemyAlly);
+        })
+
+        // choose a random target to attack
+        const attackTargetPosition = Math.floor(Math.random() * potentialTargets.length);
+        const attackTarget = targetCharacter({target: potentialTargets[attackTargetPosition]});
+
+        // attack
+        this.attack({self: this, target: charactersInBattle[attackTarget]});
     }
 }
